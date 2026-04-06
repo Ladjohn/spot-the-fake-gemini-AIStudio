41496d15e62ebaade47bb44be4c819f6654081ee
Ladjohn <158185801+Ladjohn@users.noreply.github.com>
2026-04-06 18:47:27 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 🔥 ADD THIS LINE
+import './index.css'; //
 import ErrorBoundary from './components/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
