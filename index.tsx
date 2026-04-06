29a906596a8b15ae65bd1a898438bbb9f56e346d
Ladjohn <158185801+Ladjohn@users.noreply.github.com>
2026-04-06 20:42:44 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; //
+import './index.css'; // ✅ CSS import (must be here)
 import ErrorBoundary from './components/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
